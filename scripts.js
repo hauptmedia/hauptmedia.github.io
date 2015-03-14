d89532e1a60a7bff8f8eb93acfa3b6f1a88d0480
Julian Haupt <jh@spielmeister.com>
2015-03-14 12:21:45 +0100

    added website content from live site

---
+++ scripts.js
@@ -0,0 +1,1 @@
+function toggledisplay (id){  if (document.getElementById) {    var mydiv = document.getElementById(id);    mydiv.style.display = (mydiv.style.display=='block'?'none':'block');  }}
