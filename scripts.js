519428bdd9f97e3d2dbebb68456fcde60e932448
Julian Haupt <julian.haupt@hauptmedia.de>
2015-09-22 12:53:39 +0200

    added pgp key

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-function toggledisplay (id){  if (document.getElementById) {    var mydiv = document.getElementById(id);    mydiv.style.display = (mydiv.style.display=='block'?'none':'block');  }}
+function toggledisplay (id){
+  if (document.getElementById) {
+    var mydiv = document.getElementById(id);
+    mydiv.style.display = (mydiv.style.display=='block'?'none':'block');
+  }
+}
